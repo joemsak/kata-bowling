6c8652e5832074d2dd7d3f354adc7ab6e40bd4df
Joe Sak <joe@joesak.com>
2014-06-06 11:21:28 -0400

    Remove unused function in Game

--- python/game/__init__.py
+++ python/game/__init__.py
@@ -16,18 +16,20 @@ class Game:
             else:
                 frame.append(rolls.popleft())
                 points = frame[0]
+
                 if self.__isStrike(frame):
                     points += rolls[0] + rolls[1]
                 else:
                     frame.append(rolls.popleft())
                     points += frame[1]
+
                     if self.__isSpare(frame):
                         points += rolls[0]
+
             score += points
 
         return score
 
-    def __addToScore(self, points):  self.score += points
     def __isTenthFrame(self, rolls): return len(rolls) <= 3
     def __isStrike(self, frame):     return frame[0] == 10
     def __isSpare(self, frame):      return sum(frame) == 10
