cfef66b323ed952277b3af3bb849e07de5538a9c
Joe Sak <joe@joesak.com>
2014-06-06 11:19:09 -0400

    Finish testing Game#getScore for complex bowling games

--- python/game/__init__.py
+++ python/game/__init__.py
@@ -1,3 +1,33 @@
+from collections import deque
+
 class Game:
-    def score(self, rolls):
-        return sum(rolls)
+    def getScore(self, rolls):
+        score = 0
+        frame = []
+        rolls = deque(rolls)
+
+        while len(rolls) > 0:
+            points   = 0
+            frame[:] = []
+
+            if self.__isTenthFrame(rolls):
+                points = sum(rolls)
+                rolls.clear()
+            else:
+                frame.append(rolls.popleft())
+                points = frame[0]
+                if self.__isStrike(frame):
+                    points += rolls[0] + rolls[1]
+                else:
+                    frame.append(rolls.popleft())
+                    points += frame[1]
+                    if self.__isSpare(frame):
+                        points += rolls[0]
+            score += points
+
+        return score
+
+    def __addToScore(self, points):  self.score += points
+    def __isTenthFrame(self, rolls): return len(rolls) <= 3
+    def __isStrike(self, frame):     return frame[0] == 10
+    def __isSpare(self, frame):      return sum(frame) == 10
--- python/tests/game_tests.py
+++ python/tests/game_tests.py
@@ -3,8 +3,21 @@ from game import Game
 
 game = Game()
 
-def test_all_gutters_scores_zero():
-    assert_equal(0, game.score([0] * 20))
-
 def test_all_ones_scores_twenty():
-    assert_equal(20, game.score([1] * 20))
+    assert_equal(20, game.getScore([1] * 20))
+
+def test_spare_three_spare_one_gutters_scores_twenty_five():
+    assert_equal(25, game.getScore([0, 10, 3, 7, 1] + [0] * 15))
+
+def test_strike_then_ones_then_gutters_scores_fourteen():
+    assert_equal(14, game.getScore([10, 1, 1] + [0] * 16))
+
+def test_all_strikes_scores_three_hundred():
+    assert_equal(300, game.getScore([10] * 12))
+
+def test_complex_games():
+    first_nine_frames = [10, 4, 5, 0, 9, 5, 3, 10, 10, 4, 5, 8, 2, 3, 0]
+    assert_equal(113, game.getScore(first_nine_frames))
+    assert_equal(143, game.getScore(first_nine_frames + [10, 10, 10]))
+    assert_equal(129, game.getScore(first_nine_frames + [7, 3, 6]))
+    assert_equal(122, game.getScore(first_nine_frames + [7, 2]))
