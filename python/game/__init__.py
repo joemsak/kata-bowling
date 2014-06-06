2a05c3c0ad23134ad25c99a553d350cb64814986
Joe Sak <joe@joesak.com>
2014-06-06 11:38:54 -0400

    Refactor the Game for reading

--- python/game/__init__.py
+++ python/game/__init__.py
@@ -3,32 +3,24 @@ from collections import deque
 class Game:
     def getScore(self, rolls):
         score = 0
-        frame = []
         rolls = deque(rolls)
-
         while len(rolls) > 0:
-            points   = 0
-            frame[:] = []
-
             if self.__isTenthFrame(rolls):
                 points = sum(rolls)
                 rolls.clear()
+                score += points
             else:
-                frame.append(rolls.popleft())
-                points = frame[0]
-
-                if self.__isStrike(frame):
-                    points += rolls[0] + rolls[1]
-                else:
-                    frame.append(rolls.popleft())
-                    points += frame[1]
-
-                    if self.__isSpare(frame):
-                        points += rolls[0]
-
-            score += points
+                frame  = [rolls.popleft()]
+                score += frame[0]
+                score += self.__getScoreFromFrame(rolls, frame)
 
         return score
+
+    def __getScoreFromFrame(self, rolls, frame):
+        if    self.__isStrike(frame): return rolls[0] + rolls[1]
+        else: frame.append(rolls.popleft())
+        if    self.__isSpare(frame):  return frame[1] + rolls[0]
+        else:                         return frame[1]
 
     def __isTenthFrame(self, rolls): return len(rolls) <= 3
     def __isStrike(self, frame):     return frame[0] == 10
